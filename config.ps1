fc10d080cd0b6579dba8dcda58656ccb298a6d98
orjan-berg <orjan.berg@exsitec.no>
2024-01-09 22:05:26 +0100

    added function Load-Module

--- config.ps1
+++ config.ps1
@@ -1,2 +1,23 @@
-﻿$outfile = 'C:\Program Files (x86)\Vitari\ActPrcSjekk\config.dat'
+﻿function Load-Module ($n) {
+    if (Get-Module | Where-Object { $_.name -eq $n }) {
+        Write-Host "Module $n is already imported."
+    } else {
+        if (Get-Module -ListAvailable | Where-Object { $_.name -eq $n }) {
+            Import-Module $n -Verbose
+        } else {
+            if (Find-Module -Name $n | Where-Object { $_.name -eq $n }) {
+                Install-Module -Name $n -Force -Verbose -Scope CurrentUser
+                Import-Module $n -Verbose
+            } else {
+                Write-Host "Module $n not imported, not available and not in an online gallery, exiting."
+                Exit 1
+            }
+        }
+    }
+}
+
+Load-Module 'dbatools'
+
+$outfile = 'C:\Program Files (x86)\Vitari\ActPrcSjekk\config.dat'
 (Get-Credential).Password | ConvertFrom-SecureString | Out-File $outfile
+
