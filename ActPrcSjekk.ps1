a802b3ef07a22ab0e0121b396e9b2ba8d143ea27
orjan-berg <orjan.berg@exsitec.no>
2024-01-20 23:13:00 +0100

    Added logging

--- ActPrcSjekk.ps1
+++ ActPrcSjekk.ps1
@@ -1,8 +1,3 @@
-# Import the dbatools module
-
-# Import-Module dbatools
-
-
 Function Log-Message([String]$Message) {
         Add-Content -Path '.\Log.txt' $Message
 }
@@ -23,57 +18,57 @@ $mycredential = New-Object -TypeName System.Management.Automation.PSCredential -
 $smtpServer = 'smtp.bbnett.no'
 
 
-Write-Host "Running today $today"
+$TimeStamp = (Get-Date).ToString('dd/MM/yyyy HH:mm:ss')
+Log-Message "$($TimeStamp) - Starting "
 
 # checking if dbatools is available, if not import module
 if ($module) {
         Write-Host "Module $($module.Name) - version $($module.Version) installed" -ForegroundColor Yellow
+        Set-DbatoolsInsecureConnection -SessionOnly
 } else {
         Import-Module -Name dbatools
         Write-Host 'Module dbatools imported' -ForegroundColor Yellow
+        Set-DbatoolsInsecureConnection -SessionOnly
 }
 
 
 # remove -SqlCredential to use windows authentication
-
+$TimeStamp = (Get-Date).ToString('dd/MM/yyyy HH:mm:ss')
+Log-Message "$($TimeStamp) - Connecting to $($server)"
 Connect-DbaInstance -SqlInstance $server -SqlCredential $mycredential
-
+$TimeStamp = (Get-Date).ToString('dd/MM/yyyy HH:mm:ss')
+Log-Message "$($TimeStamp) - Connected to $($server)"
 
 
 # Finner informasjon om tabellen
-
+$TimeStamp = (Get-Date).ToString('dd/MM/yyyy HH:mm:ss')
+Log-Message "$($TimeStamp) - Getting dataspace and rowcount"
 $dataSpace = (Get-DbaDbTable -SqlInstance $server -Database $database -Table $table).DataSpaceUsed
-
 $rowCount = (Get-DbaDbTable -SqlInstance $server -Database $database -Table $table).RowCount
-
-
-
-
+$TimeStamp = (Get-Date).ToString('dd/MM/yyyy HH:mm:ss')
+Log-Message "$($TimeStamp) - dataspace and rowcount collected"
 
 if ($dataSpace / $rowCount -gt 3072) {
-
         # Truncate tabellen
-
         Invoke-DbaQuery -SqlInstance $server -Database $database -Query "TRUNCATE TABLE $table" -ErrorAction Stop
-
         # Send mail
-
         $to = 'orjan.berg@exsitec.no'
-
         $from = 'noreply@bbnett.no'
-
         $subject = "Test: $table har vokst for mye"
-
         $body = "Størrelsen på tabellen $table oversteg verdien 3 megabyte per rad.  
-
         Rutinene for å redusere tabellen er kjørt."
-
         # Send-MailMessage er ikke anbefalt brukt, men jeg har ikke funnet noe annet alternativ enda
-
         Send-MailMessage -To $to -From $from -Subject $subject -Body $body -SmtpServer $smtpServer
+        $TimeStamp = (Get-Date).ToString('dd/MM/yyyy HH:mm:ss')
+        Log-Message "$($TimeStamp) - Mail sent"
 
+} else {
+        $TimeStamp = (Get-Date).ToString('dd/MM/yyyy HH:mm:ss')
+        Log-Message "$($TimeStamp) - Dataspace normal, no mail sent"
 }
 
-
 Disconnect-DbaInstance
+$TimeStamp = (Get-Date).ToString('dd/MM/yyyy HH:mm:ss')
+Log-Message "$($TimeStamp) - Disconnected from $($server)"
+
 
